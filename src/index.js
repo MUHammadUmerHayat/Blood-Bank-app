28e9cd01f399c7db6140b24205e2a4a91217debe
MUHammadUmerHayat <40881824+MUHammadUmerHayat@users.noreply.github.com>
2021-02-04 17:39:08 +0500

    initial commits

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { store } from './store'
+import routes from './routes'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './config'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+ReactDOM.render((
+    <Provider store={store}>
+        {routes}
+    </Provider>
+  ),
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
